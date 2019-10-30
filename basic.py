d40ba0b31aa934d165f6be2a953a2546551c7ae4
GK Guillen <gkguillen@GKs-MacBook-Pro.local>
2019-10-30 21:00:59 +0000

    initial push

---
+++ basic.py
@@ -0,0 +1,21 @@
+#############################
+# Basic Python Tutorials
+# Gabrielle Guillen 2019
+# Data Science and Analytics
+#############################
+
+# 1. WELCOME
+code = "Python"
+print("Hello and welcome to " + code + "! ❤️")
+
+# 2. BLOCK COMMENT
+# hashtag is the symbol for comments in Python
+
+# 3. PRINT
+print("My goal is to learn and know the basics of this language.")
+
+# 4. STRINGS
+print("My name is Gabrielle.")  # double quotes
+print('My name is Gabrielle')  # single quotes
+# correct use of single & double
+print('My name is Gabrielle but you can call me "Gabby".')
